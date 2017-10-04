a7048586052c6bc20f32b7f55e2a805d5926d23a
David Komer <david.komer@gmail.com>
2017-10-04 12:47:17 +0300

    updates

--- src/app/game/io/RendererType.ts
+++ src/app/game/io/RendererType.ts
@@ -2,7 +2,7 @@ export enum RendererType {
     BASELINE_PIXI = "Baseline-Pixi",
     REACT_EMPTY_NULL_EDGES = "React-Empty-Null",
     REACT_EMPTY_WITH_EDGES = "React-Empty-Edges",
-    REACT_PIXI_DOM = "React-Pixi-Dom",
     REACT_PIXI_CUSTOM = "React-Pixi-Custom",
+    REACT_PIXI_DOM = "React-Pixi-Dom",
     REACT_WEBGL_CUSTOM = "React-WebGl-Custom",
 }
